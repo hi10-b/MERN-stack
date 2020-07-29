335994f1f84588272c2985c47e545c700eacab09
hi10-b <41531756+hi10-b@users.noreply.github.com>
2020-07-29 21:57:22 +1000

    "POST" with axios

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import Trial from './components/Trial'
 import TopGrid from './components/TopGrid'
 import "react-bootstrap";
+import NavBar from './components/navBarComponents/NavBar';
+import Post from './components/Post';
 
 function App() {
   return (
     <div className="App">
 
-      
+        <NavBar/>
         <TopGrid/>
      
-     
+      <Post/>
     </div>
   );
 }
